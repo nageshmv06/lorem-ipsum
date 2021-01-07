3c40026ab206725b7ed246c3dfe0cc8d2c1430bb
nageshmv06 <nageshmv18@gmail.com>
2021-01-07 20:40:30 +0530

    Updates

---
+++ precache-manifest.abcf464db458d6f14c2cd7692f4e3312.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3279d515c1c9e06f9c6793f2939df55b",
+    "url": "/lorem-ipsum/index.html"
+  },
+  {
+    "revision": "006811c74cf54aebe22a",
+    "url": "/lorem-ipsum/static/css/main.9d994fcb.chunk.css"
+  },
+  {
+    "revision": "abc451bfc1dc988c8a00",
+    "url": "/lorem-ipsum/static/js/2.a167db91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lorem-ipsum/static/js/2.a167db91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006811c74cf54aebe22a",
+    "url": "/lorem-ipsum/static/js/main.c100dc4a.chunk.js"
+  },
+  {
+    "revision": "6615968d0b1ae8088015",
+    "url": "/lorem-ipsum/static/js/runtime-main.b8f9d5e5.js"
+  }
+]);
